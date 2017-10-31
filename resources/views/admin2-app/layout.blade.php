f0c9177202da4d2e34c6761a7a226413f725ed63
tanmillet <tanmillet@sina.com>
2017-10-31 16:02:36 +0800

    delete mzui file

--- resources/views/admin2-app/layout.blade.php
+++ resources/views/admin2-app/layout.blade.php
@@ -33,7 +33,8 @@
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <!-- ADD THE CLASS layout-boxed TO GET A BOXED LAYOUT -->
-<body class="hold-transition skin-blue layout-boxed sidebar-mini">
+{{--<body class="hold-transition skin-blue layout-boxed sidebar-mini">--}}
+<body class="hold-transition skin-blue fixed sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
 
